f0796f6cddea91bc4787265875bcf3fa3e24b0b1
Ethan Deng <ethan.dengzt@gmail.com>
2025-04-03 15:55:02 -0400

    added insert and remove functions

--- linked_list.c
+++ linked_list.c
@@ -2,13 +2,42 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *newNode = malloc(sizeof(struct list_node));
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  newNode -> value = value;
+  newNode -> next = NULL;
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+  return newNode; 
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *newHeadNode = new_node(value);
+  newHeadNode -> next = list -> head;
+}
+
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node * cursor = list -> head;
+  
+  while (cursor -> next != NULL) {
+    cursor = cursor -> next;
+  }
+
+  cursor -> next = new_node(value);
+}
+
+size_t remove_from_head(struct linked_list *list) {
+  if(list -> head == NULL) {
+    return 0;
+  }
+
+  struct list_node *deleteHead = list -> head;
+  list -> head = list -> head -> next; // go to the element after the head
+  size_t value = deleteHead -> value;
+  free(deleteHead); // free memory of the head node to be deleted
+
+  return value;
+}
 
 size_t remove_from_tail(struct linked_list *list) { return 0; }
 
